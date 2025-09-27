d3ee6543c7696dc7ecaa6c308cbf9a33bb0d977b
caSkds <cesar.hidalgo.rdz@comunidad.unam.mx>
2025-09-27 10:52:46 -0600

    Se añade clase alimento, se añaden métodos para cliente

---
+++ Alimento.java
@@ -0,0 +1,35 @@
+public class Alimento{
+    private String nombreAlimento;
+    private double precio;
+    private int codigoBarras;
+    private static int codigoBarrasInicial = 576341235420;
+
+    public Alimento(String nombreAlimento, double precio){
+        this.nombreAlimento = nombreAlimento;
+        this.precio = precio;
+        this.codigoBarras = codigoBarrasInicial;
+        codigoBarrasInicial++;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getNombreAlimento() {
+        return nombreAlimento;
+    }
+
+    public void setNombreAlimento(String nombreAlimento) {
+        this.nombreAlimento = nombreAlimento;
+    }
+
+    public static int getCodigoBarras() {
+        return getCodigoBarras;
+    }
+
+
+}
--- Cliente.java
+++ Cliente.java
@@ -1,15 +1,91 @@
 import java.util.ArrayList;
 public class Cliente{
     public static int idContador = 0;
-    public int id;
-    ArrayList<Libro> librosRentados = new ArrayList<>();
-    public double cuenta;
+
+    private int id;
+    private String nombre = '';
+    private ArrayList<Libro> librosRentados = new ArrayList<>();
+    private double cuenta;
 
 
-    Cliente(){
+    public Cliente(){
         idContador++;
         this.id = idContador;
     }
+    public Cliente(String nombre){
+        idContador++;
+        this.id = idContador;
+        this.nombre = nombre;
+    }
+
+    public mostrarInfo(){
+        System.out.println("ID: "+ id);
+        if (nombre.equals("")){
+            System.out.println("No se ha asignado un nombre");
+        }
+        else{
+            System.out.println(nombre);
+        }
+        System.out.println("Cuenta: "+cuenta);
+
+    }
+
+    public void pagarCuenta(double pago){
+        cuenta-=pago;
+        System.out.println("Su cuenta actual es de: "+ cuenta);
+    }
+
+    public void cobrar(double cobro){
+        cuenta+=cobro;
+        System.out.println("Su cuenta actual es de: "+ cuenta);
+    }
+
+    public void agregarLibro(Libro nuevoLibro){
+        librosRentados.add(nuevoLibro);
+    }
+    /*
+    * To do :Agregar metodo para devolver libro a inventario
+    * */
+    public void devolverLibro(int ID){
+        int contador =0;
+        for(Libro libro: librosRentados){
+            if (libro.getId()== ID){
+                libro.setEstatus("Disponible");
+                librosRentados.remove(contador);
 
 
+            }
+            contador ++;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Libro> getLibrosRentados() {
+        return librosRentados;
+    }
+
+    public void setLibrosRentados(ArrayList<Libro> librosRentados) {
+        this.librosRentados = librosRentados;
+    }
+
+    public double getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(double cuenta) {
+        this.cuenta = cuenta;
+    }
 }
