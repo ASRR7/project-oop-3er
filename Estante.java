37e76c50f72200b266b42d8de005835f219651d2
unknown <julioserrepe1@gmail.com>
2025-09-26 22:08:53 -0600

    Clase Estante, Inventario con sus métodos y atributos. A inventario agregué métodos para alimentos

---
+++ Estante.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Estante {
+    private int cantidadLibros;
+    private String genero;
+    private int pasillo;
+    private ArrayList<Libro> libros;
+
+    Estante(String genero, int pasillo){
+        this.cantidadLibros = 0;
+        this.genero = genero; //El genero y el pasillo son permanentes
+        this.pasillo = pasillo;
+        this.libros = new ArrayList<Libro>();
+    }
+
+    private void masUnoCantidadLibros(){
+        this.cantidadLibros++;
+    }
+    public int getCantidadLibros(){
+        return this.cantidadLibros;
+    }
+
+    public String getGenero(){
+        return this.genero;
+    }
+
+    public ArrayList<Libro> getLibros(){
+        return this.libros;
+    }
+
+    public int getPasillo(){
+        return this.pasillo;
+    }
+
+    public void insertarLibro(Libro libro){
+        libros.add(libro); //Añadir al final de la lista
+        this.masUnoCantidadLibros();
+    }
+    
+    public int buscarISBN(String titulo){ //Retorna el libro encontrado
+        for (Libro libro : this.getLibros()) {
+            if( titulo.equals( libro.getTitulo() ) ){
+                return libro.getISBN();
+            }
+        }
+        return -1;
+    }
+}
---
+++ Inventario.java
@@ -0,0 +1,69 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Inventario {
+    private HashMap<String, Estante> estantes;
+    private HashSet<Alimento> alimentos;
+
+    Inventario(){
+        this.estantes = new HashMap<String, Estante>();
+        this.alimentos = new HashSet<Alimento>();
+    }
+
+    public int consultarCantidad(String genero, String titulo){
+        int cantidad = 0;
+        Estante estante = this.getEstantes().get(genero);//Obtiene el estante correspondiente al genero
+        for( Libro libro : estante.getLibros() ){
+            if( libro.getTitulo().contains(titulo) ){
+                cantidad++; //Suma si el titulo buscado esta en cada libro
+            }
+        } 
+        return cantidad;//Retorna cantidad de libros
+    }
+    
+    public int consultarCantidad(String genero){
+        Estante estante = this.getEstantes().get(genero);//Obtiene el estante correspondiente al genero
+        return estante.getCantidadLibros();
+    }
+
+    public int consultarCantidadTitulo(String titulo){
+        int cantidad = 0;
+        //Ciclo que itera en el map usando entrySet para recorrer todos los hash (keys)
+        for (Map.Entry<String, Estante> estantePar : this.getEstantes().entrySet()) {
+            //Se usa el método para buscar cantidad de titulos en un estante especifico
+            cantidad += this.consultarCantidad( estantePar.getValue().getGenero(), titulo );
+        }
+        return cantidad; //Cantidad de libros en todo el inventario
+    }
+
+    public HashMap<String, Estante> getEstantes(){
+        return this.estantes;
+    }
+
+    public HashSet<Alimento> getAlimentos(){
+        return this.alimentos;
+    }
+
+    public Alimento hayAlimento(String alimentoNombre){
+        for ( Alimento alimento : this.getAlimentos() ) {
+            if( alimentoNombre.equals( alimento.getNombre() ) ){
+                return alimento;
+            }
+        }
+    }
+
+    public void quitarAlimento(String alimentoNombre){
+        Alimento alimento = this.hayAlimento(alimentoNombre);
+        this.getAlimentos().remove(alimento);
+    }
+
+    public void mostrarAlimentos(){
+        System.out.println("Alimentos en inventario: ");
+        for ( Alimento alimento : this.getAlimentos() ) {
+            System.out.print(alimento + ", ");
+        }
+        System.out.println("\n");
+    }
+}
