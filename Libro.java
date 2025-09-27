bb1edf38499e5357b67cff53b16289e4052830a6
caSkds <cesar.hidalgo.rdz@comunidad.unam.mx>
2025-09-26 19:32:25 -0600

    Atributos de Libro agregados

--- Libro.java
+++ Libro.java
@@ -1,6 +1,18 @@
 public class Libro{
+
+    public static int bookCount = 0;
+
     public String Titulo;
     public String Autor;
+    public String Genero;
+    public String Editorial;
+    public int ISBN;
+    public String Status;
+    public Double Precio;
+    public Boolean enInventario;
+    public int id;
+
+
 
 
 }
