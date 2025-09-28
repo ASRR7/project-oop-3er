eae24897a18cec4a704b1bbfc2b20e271702287b
Ivan <andreey609@gmail.com>
2025-09-28 15:29:35 -0600

    Solo pa actualizar

--- Principal.java
+++ Principal.java
@@ -15,6 +15,11 @@ public class Principal {
         System.out.println("1.");
         System.out.println("1.");
         System.out.println("6. Salir");
-
+        switch (opcion){
+            case 1: 
+                System.out.println("Hola");
+            case 6: 
+                System.out.println("Saliendo");
+        }
     } while (opcion != 6);
 }
