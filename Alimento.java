09f6c4a063a391f8671448f477029b9ef6c11825
Ivan <andreey609@gmail.com>
2025-09-28 22:16:42 -0600

    Javadoc de Alimento

--- Alimento.java
+++ Alimento.java
@@ -3,32 +3,58 @@ public class Alimento{
     private double precio;
     private int codigoBarras;
     private static int codigoBarrasInicial = 576235420;
-
+    /**
+     * <h2>Constructor Alimento</h2>
+     * <p>
+     * Inicializa un objeto de tipo Alimento asignándole nombre, precio y un
+     * código de barras único generado automáticamente.
+     *
+     * @param nombre el nombre del alimento
+     * @param precio el precio del alimento
+     * @author Equipo 13
+     * @version 1.0
+     * @see Alimento
+     */
     public Alimento(String nombre, double precio){
         this.nombre = nombre;
         this.precio = precio;
         this.codigoBarras = codigoBarrasInicial;
         codigoBarrasInicial++;
     }
-
+    /**
+     * Devuelve el precio del alimento.
+     * @return precio del alimento
+     */
     public double getPrecio() {
         return precio;
     }
-
+    
+    /**
+     * Hace autoreferencia al precio del alimento para modificarlo. 
+     * @param precio nuevo del alimento
+     */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
-
+    /**
+     * Devuelve el nombre del alimento.
+     * @return nombre del alimento
+     */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * Hace autoreferencia al nombre del alimento para modificarlo.
+     * @param nuevo nombre del alimento
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     * Devuelve el código de barras del alimento.
+     * @return código de barras del alimento.
+     */
     public int getCodigoBarras() {
         return codigoBarras;
     }
-
 }
